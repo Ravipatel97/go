bb27e40ece8c1e4ce41e9c1db367757f243685df
Ravipatel97 <34805227+Ravipatel97@users.noreply.github.com>
2020-04-25 13:47:01 +0530

    Update main.go

--- final/main.go
+++ final/main.go
@@ -1,19 +1,16 @@
 package main
 
 import (
-	"encoding/json"
+	"bufio"
 	"fmt"
 	"golang.org/x/crypto/bcrypt"
 	"html/template"
-	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 )
-type name struct {
-	username, email string
-	psword          string
-}
+
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm() //Parse url parameters passed, then parse the response packet for the POST body (request body)
 	// attention: If you do not call ParseForm method, the following data can not be obtained form
@@ -25,20 +22,52 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("key:", k)
 		fmt.Println("val:", strings.Join(v, ""))
 	}
-	fmt.Fprintf(w, "Hello astaxie!") // write data to response
+	fmt.Fprintf(w, "Hello ") // write data to response
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("method:", r.Method) //get request method
 	if r.Method == "GET" {
 		t, _ := template.ParseFiles("templates/login.html")
-			t.Execute(w, nil)
+		t.Execute(w, nil)
 	} else {
 		r.ParseForm()
 		// logic part of log in
+		pswd := r.FormValue("password")
+		flag := verify(pswd)
+		if flag == true {
+			fmt.Fprintf(w, "success")
+		}
+
 		fmt.Println("username:", r.Form["username"])
 		fmt.Println("password:", r.Form["password"])
 	}
+}
+func verify(pswd string) bool {
+	file, err := os.Open("test.txt")
+	var flag bool
+	if err != nil {
+		log.Fatalf("failed opening file: %s", err)
+	}
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	file.Close()
+	for _, l := range lines {
+		strarray := strings.Fields(l)
+		bytehash := []byte(strarray[2])
+		err := bcrypt.CompareHashAndPassword([]byte(pswd),bytehash)
+		if err != nil {
+			log.Println(err)
+			flag=true
+			break
+		}
+		flag=false
+	}
+	return flag
 }
 func register(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("method:", r.Method)
@@ -48,23 +77,25 @@ func register(w http.ResponseWriter, r *http.Request) {
 	} else {
 		r.ParseForm()
 		// logic part of log in
-		uName:= r.FormValue("username")
-		email:= r.FormValue("email")
-		pwd:= r.FormValue("password")
-		pwdConfirm:= r.FormValue("confirmpassword")
+		uName := r.FormValue("username")
+		email := r.FormValue("email")
+		pwd := r.FormValue("password")
+		pwdConfirm := r.FormValue("confirmpassword")
 		if pwd == pwdConfirm {
 			hash, err := bcrypt.GenerateFromPassword([]byte(pwd), bcrypt.DefaultCost)
 			if err != nil {
-				// TODO: Properly handle error
 				log.Fatal(err)
 			}
-			fmt.Println("hash",string(hash))
-			data:=name{username: uName,email: email,psword:string(hash) }
-			fmt.Println("data",data)
-			file, _ := json.MarshalIndent(data, "", " ")
-
-			_ = ioutil.WriteFile("test.json", file, 0644)
-			_, _ = fmt.Fprintln(w, "Registration successful.")
+			fmt.Println("hash", string(hash))
+			data := []string{uName, email, string(hash)}
+			data1 := strings.Join(data, " ")
+			f, err := os.OpenFile("test.txt", os.O_APPEND|os.O_WRONLY, 0644)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			_, err = fmt.Fprintln(f, data1)
+			http.Redirect(w, r, "/login", 301)
 		} else {
 			fmt.Fprintln(w, "Password information must be the same.")
 		}
@@ -73,6 +104,7 @@ func register(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", sayhelloName) // setting router rule
 	http.HandleFunc("/login", login)
+	//http.HandleFunc("/index", index)
 	http.HandleFunc("/register", register)
 	err := http.ListenAndServe(":9090", nil) // setting listening port
 	if err != nil {
